74c8cab14afa5401b71e9ed1f15f87ec001ab555
Mike Apted <mikapted@amazon.com>
2021-01-25 13:03:55 -0500

    Initial commit

--- cdk_my_api_python/cdk_my_api_python_stack.py
+++ cdk_my_api_python/cdk_my_api_python_stack.py
@@ -1,9 +1,32 @@
-from aws_cdk import core
-
+from aws_cdk import (
+    aws_lambda as _lambda,
+    aws_apigatewayv2 as apigv2,
+    aws_apigatewayv2_integrations as apigv2int,
+    core,
+)
+import os
 
 class CdkMyApiPythonStack(core.Stack):
 
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        my_api_function = _lambda.DockerImageFunction(self, 'MyApiFunction',
+            code = _lambda.DockerImageCode.from_image_asset('../src'),
+        )
+
+        my_default_integration = apigv2int.LambdaProxyIntegration(
+            handler = my_api_function,
+        )
+
+        my_http_api = apigv2.HttpApi(self, 'MyApi',
+            default_integration = my_default_integration
+        )
+
+        core.CfnOutput(self, 'MyApiUrl',
+            value = my_http_api.api_endpoint
+        )
+        
+        core.CfnOutput(self, 'MyApiFnLogGroup',
+            value = my_api_function.log_group.log_group_name
+        )
