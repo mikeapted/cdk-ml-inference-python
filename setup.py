91a87eefaf11d503fa0899a03d81760648ed3b16
Mike Apted <mikapted@amazon.com>
2021-01-25 14:56:04 -0500

    Update requirements

--- setup.py
+++ setup.py
@@ -20,6 +20,9 @@ setuptools.setup(
 
     install_requires=[
         "aws-cdk.core==1.86.0",
+        "aws-cdk.aws-lambda==1.86.0",
+        "aws-cdk.aws_apigatewayv2==1.86.0",
+        "aws-cdk.aws_apigatewayv2_integrations==1.86.0"
     ],
 
     python_requires=">=3.6",
